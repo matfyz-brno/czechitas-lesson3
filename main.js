4fc00c2d7251f3fe3237638c2aa8d286eb856ce8
Eva Vašicová <miev@seznam.cz>
2023-02-02 20:22:55 +0100

    zmena z hodiny

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-// tady je místo pro náš program
+/*let title = document.querySelector("h1");
+let vstup_uzivatele = prompt("Zadej nový nadpis");
+title.innerHTML = vstup_uzivatele; 
+
+let result = document.querySelector(".result")
+const start = 15;
+const delka = Number(prompt('Zadej délku závodu:'));
+const konec = (start + delka) % 24;
+result.innerHTML = konec; */
+
+/*let user_name = prompt("Jaké je Vaše jméno a příjmení?: ");
+let user_age = Number(prompt("Jaký je Váš věk?: "));
+let user_name_age = document.querySelector(".name_age");
+
+user_name_age.innerHTML = "Uživatel " + user_name + " má věk " 
+ + user_age; */
+
+let hour_salary = Number(prompt(" Jaká je Vaše hodinová mzda v korunách? :"));
+let complete_salary = (hour_salary * 8 * 21);
+let salary = document.querySelector(".salary")
